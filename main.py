5d74ecac365ac7f099278ec6444ec775e5b3ed53
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-06-04 17:15:26 -0300

    Add command line args

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import cv2
 import sys
 from tqdm import tqdm
 import imutils
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--skip-frames", default=10, type=int)
+parser.add_argument("--secs-refetch", default=1, type=int)
+parser.add_argument("--confidence", default=0, type=int)
+parser.add_argument("--overlap", default=0, type=int)
+parser.add_argument("video")
+args = parser.parse_args()
 
 rf = Roboflow(api_key="aVZZ7kyDSgHYfCJt0DCr")
 project = rf.workspace().project("billboards-cmzpu")
@@ -45,23 +54,17 @@ OPENCV_OBJECT_TRACKERS = {
 
 trackers = cv2.legacy.MultiTracker_create()
 
-# model.predict(
-#     "test.jpg",
-#     confidence=0,
-#     overlap=0,
-# ).save("roboflow.jpg")
-
-# img = cv2.imread("test.jpg")
-video = cv2.VideoCapture("test.mkv")
+video = cv2.VideoCapture(args.video)
 fps = video.get(cv2.CAP_PROP_FPS)
-width = int(640)
-height = int(480)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-outvid = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
+width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+outvid = cv2.VideoWriter(f'out{args.video}', fourcc, fps, (width, height))
 numberframes = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 for i in tqdm(range(numberframes)):
-    if (i != 48 and i % 10 == 0):
+    if (args.skip_frames and i % args.skip_frames == 0):
         continue
     ret, frame = video.read()
     if not ret:
@@ -70,11 +73,11 @@ for i in tqdm(range(numberframes)):
         break
     frame = cv2.resize(frame, (width, height))
 
-    if i % 48 == 0:
+    if i % (args.secs_refetch * int(fps)) == 1:
         results = model.predict(
             frame,
-            confidence=0,
-            overlap=0,
+            confidence=args.confidence,
+            overlap=args.overlap,
         )
         trackers = cv2.legacy.MultiTracker_create()
         for bbox in [objToBbox(result) for result in results]:
@@ -85,20 +88,8 @@ for i in tqdm(range(numberframes)):
         for box in boxes:
             x1, y1, x2, y2 = bboxToObject(box)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-    # for obj in results:
-    #     x1 = int(x1)
-    #     y1 = int(y1)
-    #     x2 = int(x2)
-    #     y2 = int(y2)
-    #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    # cv2.putText(img, f"Billboard {round(obj['confidence']*100, 2)}%",
-    #             (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (36, 255, 12), 2)
-
+            # Como escribimos la confidence aca?
     outvid.write(frame)
-    # cv2.imshow("image", frame)
-    # if (cv2.waitKey(0)):
-    #     break
 
 video.release()
 outvid.release()
