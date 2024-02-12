e9a4ffe8254f7f7a475f4243be3b4666e9fe2b20
pastine <casimiropastine@gmail.com>
2024-02-12 09:58:06 -0300

    Re-write core.py's detect function to work with ONNX models

--- src/core.py
+++ src/core.py
@@ -3,16 +3,39 @@ import os
 import cv2
 import cvutils
 import numpy as np
+from collections import defaultdict
 
 IMAGE_SIZE = 640
+CLASSES = defaultdict(lambda: "Unknown")
+CLASSES[0] = "Billboard"
+CLASSES[45] = "Vaso"
+colors = np.random.uniform(0, 255, size=(600, 3))
+
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    """
+    Draws bounding boxes on the input image based on the provided arguments.
+
+    Args:
+        img (numpy.ndarray): The input image to draw the bounding box on.
+        class_id (int): Class ID of the detected object.
+        confidence (float): Confidence score of the detected object.
+        x (int): X-coordinate of the top-left corner of the bounding box.
+        y (int): Y-coordinate of the top-left corner of the bounding box.
+        x_plus_w (int): X-coordinate of the bottom-right corner of the bounding box.
+        y_plus_h (int): Y-coordinate of the bottom-right corner of the bounding box.
+    """
+    label = f"{CLASSES[class_id]} ({confidence:.2f})"
+    color = colors[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 def get_model():
     cwd = os.path.dirname(os.path.realpath(__file__))
     net = cv2.dnn.readNet(
         # YOLOv8 ONNX Model
-        # f"{cwd}/model/billboard-detection/best.onnx",
+        f"{cwd}/model/billboard-detection/best.onnx",
         # YOLOv8 Pre-Trained Model
-        f"{cwd}/model/billboard-detection/yolov8n.onnx",
+        # f"{cwd}/model/billboard-detection/yolov8n.onnx",
         # Caffe Model
         # f"{cwd}/model/face-detection/res10_300x300_ssd_iter_140000.caffemodel",
         # f"{cwd}/model/face-detection/deploy.prototxt",
@@ -24,51 +47,73 @@ def get_model():
 def detect(frame, confidence=0.2, model=get_model()):
     # Feed the frame to the model
     net, net_dimensions = model
-    blob = cv2.dnn.blobFromImage(frame, 1.0, net_dimensions)
+
+    # Read the input image
+    original_image = frame
+    [height, width, _] = original_image.shape
+
+    # Prepare a square image for inference
+    length = max((height, width))
+    image = np.zeros((length, length, 3), np.uint8)
+    image[0:height, 0:width] = original_image
+
+    # Calculate scale factor
+    scale = length / 640
+
+    # Preprocess the image and prepare blob for model
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
     net.setInput(blob)
 
-    # Run the model itself
-    detections = net.forward()
+    # Perform inference
+    outputs = net.forward()
 
-    billboardDetection = detections[0]
-    print('First detection', billboardDetection[0:5, 0])
-    print('\nSTART FRAME\n')
-    # mostly taken from https://pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/
-    # loop over the detections
-    for i in range(billboardDetection.shape[1]):
-        current_detection = billboardDetection[0:5, i]
-        # print('current_detection', current_detection)
-        # extract the confidence associated with the prediction
-        detection_confidence = current_detection[4]
+    outputs = np.array([cv2.transpose(outputs[0])])
+    rows = outputs.shape[1]
 
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if not detection_confidence > confidence:
-            continue
-        print('\ndetection_confidence', detection_confidence)
+    boxes = []
+    scores = []
+    class_ids = []
 
-        # compute the (x, y)-coordinates of the bounding box for the object
-        # multiply by the frame size to get the actual coordinates
-        height, width = frame.shape[:2]
-        # print('height', height)
-        # print('width', width)
+    # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+    for i in range(rows):
+        classes_scores = outputs[0][i][4:]
+        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+        if maxScore >= 0.20:
+            box = [
+                outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                outputs[0][i][2],
+                outputs[0][i][3],
+            ]
+            boxes.append(box)
+            scores.append(maxScore)
+            class_ids.append(maxClassIndex)
 
-        (startX, startY, endX, endY) = (
-            current_detection[0:4] * np.array([width, height, width, height])
-        ).astype("int")
-        print('startX', startX)
-        print('startY', startY)
-        print('endX', endX)
-        print('endY', endY)
-        print('\n')
+    # Apply NMS (Non-maximum suppression)
+    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
 
-        # draw the bounding box along with the associated probability
-        cvutils.draw(
-            frame,
-            # (startX, startY, endX, endY),
-            current_detection[0:4].astype("int"),
-            "{:.2f}%".format(detection_confidence * 100),
+    detections = []
+
+    # Iterate through NMS results to draw bounding boxes and labels
+    for i in range(len(result_boxes)):
+        index = result_boxes[i]
+        box = boxes[index]
+        detection = {
+            "class_id": class_ids[index],
+            "class_name": CLASSES[class_ids[index]],
+            "confidence": scores[index],
+            "box": box,
+            "scale": scale,
+        }
+        detections.append(detection)
+        draw_bounding_box(
+            original_image,
+            class_ids[index],
+            scores[index],
+            round(box[0] * scale),
+            round(box[1] * scale),
+            round((box[0] + box[2]) * scale),
+            round((box[1] + box[3]) * scale),
         )
 
-    print('\nEND FRAME\n')
     return frame
