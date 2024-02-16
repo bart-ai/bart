6fe1006f7b79b93a55cae4860728979887f563a9
Federico del Mazo <fdelmazo@fi.uba.ar>
2024-02-16 19:36:50 -0300

    Show how much time was spent on each frame

--- src/web.py
+++ src/web.py
@@ -1,3 +1,6 @@
+import queue
+import time
+
 import av
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
@@ -18,15 +21,19 @@ def cached_get_model():
 
 
 model = cached_get_model()
+time_in_frames = queue.Queue()
 
 
 def call_detect(frame):
     img = frame.to_ndarray(format="bgr24")
+    start_time = time.time()
     img = model.detect(img, transformation=transformation, confidence=confidence / 100)
+    end_time = time.time()
+    time_in_frames.put(end_time - start_time)
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-webrtc_streamer(
+webrtrc_ctx = webrtc_streamer(
     # https://github.com/whitphx/streamlit-webrtc#serving-from-remote-host
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     video_frame_callback=call_detect,
@@ -41,6 +48,7 @@ webrtc_streamer(
 )
 
 with st.expander("Configuration"):
+    time_container = st.empty()
     transformation = st.selectbox(
         "Transformation",
         options=("detect", "blur"),
@@ -49,3 +57,10 @@ with st.expander("Configuration"):
     confidence = st.slider(
         "Detection score", min_value=0, max_value=100, value=80, step=5
     )
+
+
+# Everything after this block won't be run.
+# Make sure this is at the end of the file.
+while webrtrc_ctx.state.playing:
+    result = time_in_frames.get()
+    time_container.text(f"Frame processing time: {result:.3f} seconds")
