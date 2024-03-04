307b27cf50931a8bcff7c45616535f3d87ac99e3
Federico del Mazo <fdelmazo@fi.uba.ar>
2024-03-04 12:54:07 -0300

    Remove unused script.py

--- src/cvutils.py
+++ src/cvutils.py
@@ -1,28 +1,5 @@
 import cv2
 
-
-# Check if there's any overlap between two boxes
-# TODO: add threshold parameter
-def overlap(box1, box2):
-    (startX1, startY1, width1, height1) = box1
-    (startX2, startY2, width2, height2) = box2
-    x = max(startX1, startX2)
-    y = max(startY1, startY2)
-    w = min(startX1 + width1, startX2 + width2) - x
-    h = min(startY1 + height1, startY2 + height2) - y
-    return w > 0 or h > 0
-
-
-# Convert a (startx, starty, width, height) tuple to a (startx, starty, endx, endy) tuple
-def trackerToBbox(startX, startY, width, height):
-    return (int(startX), int(startY), int(startX + width), int(startY + height))
-
-
-# Convert a (startx, starty, endx, endy) tuple to a (startx, starty, width, height) tuple
-def bboxToTracker(startX, startY, endX, endY):
-    return (int(startX), int(startY), int(endX - startX), int(endY - startY))
-
-
 # Draw a rectangle on the frame, with optional text
 def draw(frame, rectangle, text=None, color=(0, 255, 0), thickness=2):
     (startX, startY, endX, endY) = rectangle
