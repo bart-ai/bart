ab3461b3e60deaf6d952b4481d4d2584101df571
Federico del Mazo <fdelmazo@fi.uba.ar>
2024-03-07 19:52:46 -0300

    Add background image downloader script

---
+++ yolo8/datasets/negative-images.py
@@ -0,0 +1,48 @@
+# Adapted from https://y-t-g.github.io/tutorials/bg-images-for-yolo/
+# Adapted from https://stackoverflow.com/a/62770484/8061030
+import os
+
+import requests
+from pycocotools.coco import COCO
+
+# We need to exclude everything that __might__ have a billboard in it
+# http://github.com/ultralytics/yolov5/blob/df48c205c5fc7be5af6b067da1f7cb3efb770d88/data/coco.yaml
+DETECTOR_CLASSES = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "tv",
+]  
+
+DIRECTORY_STRUCTURE = ["negative-images", "train", "images"]
+NUM_IMAGES = 300
+
+# http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+coco = COCO("instances_train2017.json")
+
+exc_cat_ids = coco.getCatIds(catNms=DETECTOR_CLASSES)
+exc_img_ids = coco.getImgIds(catIds=exc_cat_ids)
+
+all_cat_ids = coco.getCatIds(catNms=[""])
+all_img_ids = coco.getImgIds(catIds=all_cat_ids)
+
+bg_img_ids = set(all_img_ids) - set(exc_img_ids)
+bg_images = coco.loadImgs(bg_img_ids)
+
+os.makedirs(os.path.join(*DIRECTORY_STRUCTURE), exist_ok=True)
+
+for im in bg_images[:NUM_IMAGES]:
+    img_data = requests.get(im["coco_url"]).content
+    with open(os.path.join(*[*DIRECTORY_STRUCTURE, im["file_name"]]), "wb") as handler:
+        handler.write(img_data)
