907c6aab2f4ae840376e43d37e85700735fe788d
pastine <casimiropastine@gmail.com>
2024-04-06 18:33:06 -0300

    test another thing

--- src/web.py
+++ src/web.py
@@ -24,7 +24,7 @@ print(f"\nContents: {os.listdir(BILLBOARD_MODELS_DIR)}")
 
 
 billboard_models = [
-    # model.replace(".onnx", "") for model in os.listdir(BILLBOARD_MODELS_DIR)
+    model.replace(".onnx", "") for model in os.listdir(BILLBOARD_MODELS_DIR)
 ]
 
 # The general layout of the app: title -> webcam -> config panel
