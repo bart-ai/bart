c06048514692b0ec918a8051d3b697097ec260a8
Federico del Mazo <FdelMazo@fi.uba.ar>
2024-05-01 12:36:36 -0300

    Set up final datasets and names

--- yolo8/datasets/download-datasets.py
+++ yolo8/datasets/download-datasets.py
@@ -3,15 +3,28 @@ import os
 from roboflow import Roboflow
 
 roboflow = Roboflow(os.environ["ROBOFLOW_API_KEY"])
+# Criteria:
+# only use datasets with train and val (test isn't required)
+# only use datasets with 640x640 images
+# don't train with less than 1500 training images
 datasets = [
-    ("image-processing-awivd/billboards-4zz9y", 3),
-    ("vehicle-yolov5/billboard-test", 2),
-    ("test-c8wix/billboard-detection-uo2ld", 3),
-    ("billboards/billboards-cmzpu", 3),
-    ("computer-vision-project-ixips/billboards-detection", 3),
-    ("hadplus/hadplus-illegal-billboard-detector", 1),
-    ("oleksii-kurochkin/billboard", 1),
-    ("billboard-asmlb/billboard-isfau", 2),
+    # multi
+    ("computer-vision-project-ixips/billboards-detection", 3),  # 389
+    ("billboards/billboards-cmzpu", 3),  # 211
+    ("hadplus/hadplus-illegal-billboard-detector", 1),  # 270
+    ("image-processing-awivd/billboards-4zz9y", 3),  # 400
+    ("image-processing-awivd/bills0", 1),  # 530
+    ("dip-9/billboards-oopxm", 2),  # 714
+    # hackaton
+    ("projet-sia/hackathon-03-2024", 1),  # 4503
+    # isfau
+    ("billboard-asmlb/billboard-isfau", 2),  # 7137
+    # uo2ld
+    ("test-c8wix/billboard-detection-uo2ld", 3),  # 1903
+    # ydtns
+    ("kaliani/bb-ydtns", 1),  # 2719
+    # s2oeb
+    ("dream-labs/billboard-s2oeb", 1),  # 8155
 ]
 
 for dataset, version in datasets:
